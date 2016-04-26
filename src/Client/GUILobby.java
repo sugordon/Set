fa655792a34465c99dff4e2134148c7aaddd13da
Bridget <bridget.bissell@gmail.com>
2016-04-26 13:06:53 -0400

    Editing GUILobby

--- src/Client/GUILobby.java
+++ src/Client/GUILobby.java
@@ -5,13 +5,14 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by Bridget on 4/23/2016.
  */
 
 public class GUILobby extends JPanel{
-/*
     public JFrame lobby = new JFrame("Lobby");
 
     private JPanel games = new JPanel();
@@ -48,15 +49,15 @@ public class GUILobby extends JPanel{
 
         formatTable(gameTable);
         JScrollPane scrollPane = new JScrollPane(gameTable);
-        scrollPane2.setPreferredSize(new Dimension(COL_WIDTH, 200));
+        scrollPane.setPreferredSize(new Dimension(COL_WIDTH, 200));
         JLabel gamesLabel = new JLabel("Games Listing");
         gamesLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         games.add(gamesLabel);
         games.add(scrollPane);
 
         JPanel temp = new JPanel();
-        temp.add(refreshGames);
-        temp.add(createButton);
+        temp.add(refreshGameButton);
+        temp.add(createGameButton);
         temp.setAlignmentX(Component.CENTER_ALIGNMENT);
         temp.setMaximumSize(new Dimension(COL_WIDTH, 10));
         games.add(temp);
@@ -65,29 +66,48 @@ public class GUILobby extends JPanel{
         addListeners();
         refreshGameButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
-                clientConnection.update_game_list();
+                update_game_list();
             }
         });
         logoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
-                clientConnection.disconnect_from_server();
+                disconnect_from_server();
             }
         });
         createGameButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
-                clientConnection.create_game(myID, nameField.getText(), passField.getText(), 5);
+                create_game(myID, nameField.getText(), passField.getText(), 5);
             }
         });
     }
 
     private void addListeners(){
+        lobby.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                //TODO: Add proper exit behavior (take back to login)
+                super.windowClosing(e);
+            }
+        });
+    }
+
+    private void update_game_list(){
+        //GORDON FILL IN
+    }
+
+    private void disconnect_from_server(){
+        //GORODON FILL IN
+        //After you log out, takes you back to login screen
+    }
+
+    private void create_game(){
 
     }
 
-    /*private JPanel getGameCreationPanel(){
+    private JPanel getGameCreationPanel() {
         JPanel temp = new JPanel();
-        temp.add(Box.createRigidArea(new Dimension(0,20)));
-        temp.add(Box.createRigidArea(new Dimension(20,0)));
+        temp.add(Box.createRigidArea(new Dimension(0, 20)));
+        temp.add(Box.createRigidArea(new Dimension(20, 0)));
         temp.setLayout(new BoxLayout(temp, BoxLayout.Y_AXIS));
         temp.add(new JLabel("<html><h1>Create Game</h1></html>"));
         JPanel names = new JPanel();
@@ -97,9 +117,8 @@ public class GUILobby extends JPanel{
         names.add(passField);
         temp.add(names);
         temp.add(Box.createVerticalGlue());
-        temp.add(createGame);
+        temp.add(createGameButton);
         return temp;
     }
-    */
 
 }
