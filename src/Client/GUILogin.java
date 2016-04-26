b74c51a380a98a48d8f74f57fabe2cedfe7b7486
Venkat Kuruturi <kurutu@cooper.edu>
2016-04-26 04:00:19 -0400

    added windowlistener to handle closing the panel.
    TODO: need to fill in the windowlistener function

--- src/Client/GUILogin.java
+++ src/Client/GUILogin.java
@@ -3,10 +3,7 @@ package Client;
 import javax.swing.*;
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusListener;
+import java.awt.event.*;
 import java.security.MessageDigest;
 
 /**
@@ -79,7 +76,13 @@ public class GUILogin extends JPanel{
         loginscreen.add(userText);
         loginscreen.add(passwordText);
 
-
+        loginscreen.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                //TODO: Add proper exit behavior
+                super.windowClosing(e);
+            }
+        });
 
         loginscreen.setVisible(true);
         loginscreen.pack();
