525c8c4af369429b7ab8a009f2de974272453e0d
Gordon Su <gordonsu14@gmail.com>
2016-04-28 01:08:42 -0400

    Fixed cards

--- src/game/Card.java
+++ src/game/Card.java
@@ -3,24 +3,24 @@ package game;
 import javax.swing.*;
 
 public class Card extends JToggleButton{
-//	final int number;
-//	final int symbol;
-//	final int shading;
-//	final int color;
+	final int number;
+	final int symbol;
+	final int shading;
+	final int color;
     final int hash;
+
+    public Card(String input) {
+        this(input.charAt(0) - '0', input.charAt(1) - '0', input.charAt(2) - '0', input.charAt(2) - '0');
+    }
 
 	public Card(int num, int sym, int sha, int col) {
         this.hash = 1000*num+ 100*sym+ 10*sha+ col;
-//		this.number = num;
-//		this.symbol = sym;
-//		this.shading = sha;
-//		this.color = col;
+		this.number = num;
+		this.symbol = sym;
+		this.shading = sha;
+		this.color = col;
 	}
 
-    public Card(String input) {
-        hash = Integer.parseInt(input);
-    }
-	
 	@Override
 	public int hashCode() {
 		return hash;
